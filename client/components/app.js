a431928e3769b4a45d962f2baaae8893a1f8f86c
Gary <garyguan0713@gmail.com>
2019-06-17 15:05:42 -0700

    Implemnt APP.js

--- client/components/app.js
+++ client/components/app.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import FileReader from './FileReader';
+import Diagram from './Diagram';
 
 class App extends React.Component{
-  constructor(){
-    super()
-    this.state={}
+  constructor() {
+    super();
+    this.state = {};
+    this.updateData = this.updateData.bind(this);
   }
-  render(){
-    return(
-      <div>Hello</div>
-    )
+
+  updateData(result) {
+    var data = result.data;
+    data.forEach((row)=>{
+      let { category_id, company_id, amount } = row;
+      let category = this.state[category_id] ? this.state[category_id] : [0];
+      category[0] += Number(amount);
+      this.setState({
+        [category_id]: category
+      })
+    })
+    let arr = Object.keys(this.state).sort((a,b)=>a[0]-b[0])
+    console.log(this.state["1"]);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FileReader updateData={this.updateData}/>
+        <Diagram />
+      </div>
+    );
   }
 }
 

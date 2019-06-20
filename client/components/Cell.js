30beea514556fa3f03e3136fdbc85ccc70afbfb7
Gary <garyguan0713@gmail.com>
2019-06-19 21:47:28 -0700

    Refactor min size in cell

--- client/components/Cell.js
+++ client/components/Cell.js
@@ -6,20 +6,22 @@ const Cell = props => {
   const [ cellWidth, cellHeight ] = size;
   let [ currWidth, currHeight] = curr;
   const [ width, height ] = boardSize;
-  const [ companies, groupSize ] = data;
+  const [ companies ] = data;
   const colors = colors;
   if(cellWidth !== currWidth) currWidth += cellWidth;
   if(cellHeight !== currHeight) currHeight += cellHeight;
   const span = companies.map((company, index)=>
-    <div className="company"
-    style={{  
-      width: cellWidth > cellHeight ? cellWidth/companies.length - 2: cellWidth - 2,
-      height: cellHeight > cellWidth ? cellHeight/companies.length -2 : cellHeight - 2,
-      background: colorGenerate(),
-    }}>
+    <div 
+      className="company"
+      key={index}
+      style={{  
+        width: cellWidth > cellHeight ? cellWidth/companies.length - 2: cellWidth - 2,
+        height: cellHeight > cellWidth ? cellHeight/companies.length -2 : cellHeight - 2,
+        background: colorGenerate(),
+      }}>
         <p style={{
-          // marginTop: cellWidth > cellHeight? (0.0025*cellWidth).toString() + 'vw' : (0.02*cellHeight).toString() + 'vh',
-          fontSize: cellWidth > cellHeight? (0.0055*cellWidth).toString() + 'vw' : (0.02*cellHeight).toString() + 'vh',
+          marginTop: cellWidth > cellHeight? (0.0025*cellWidth).toString() + 'vw' : (0.02*cellHeight).toString() + 'vh',
+          fontSize: cellWidth > cellHeight? (0.0055*cellWidth).toString() + 'vw' : (0.015*cellHeight).toString() + 'vh',
           margin: 0,
           display: 'inline-block',
           whiteSpace: 'nowrap',
